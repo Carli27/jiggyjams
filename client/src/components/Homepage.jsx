1a473292e563d1044ee564f4153a46b0e41b0fc1
Carli27 <cmccarthy27@hotmail.com>
2023-08-24 23:57:40 +0100

    -mhomepage

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import homepage_scotch from "../assets/homepage_scotch.png";
+import { Link } from "react-router-dom"; // need to import the link
 
 export default function Homepage() {
   return (
     <div className="container">
       <div className="row">
         <div className="col">Homepage</div>
+        <img
+          src={homepage_scotch}
+          alt="scotch bonnet chilli jam"
+          className="img-fluid"
+        />
+      </div>
+      <div className="col d-flex justify-content-center align-items-center">
+        <Link to={`/products`}>
+          <button
+            type="button"
+            className="btn btn-secondary btn-lg btn btn-dark"
+          >
+            SHOP THE JAM
+          </button>
+        </Link>
       </div>
     </div>
   );
 }
+
+/*To do
+- change products url to shop 
+*/
